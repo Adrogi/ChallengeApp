5ab64e87524b2875f3ff9078e9a89a28b9b62cf2
adrog <adroginski@gmail.com>
2023-01-17 22:41:21 +0100

    Dzień 4 - Praca domowa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Michał";
+string maleSuffix = "-latek";
+string femaleSuffix = "-latka";
+bool woman = false;
+var age = 13;
+
+
+if (woman && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (name != "Ewa" && woman)
+{
+    Console.WriteLine(age + femaleSuffix + " o imieniu " + name);
+}
+else if (name != "Ewa" && !woman)
+{
+    Console.WriteLine(age + maleSuffix + " o imieniu " + name);
+}
+else if ( age < 18 && !woman)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine(" Pełnoletni Mężczyzna");
+}
+
+
+
+
