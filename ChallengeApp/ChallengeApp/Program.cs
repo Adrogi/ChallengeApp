a91d915ec10163afdc8a5121aa65e87a5e7cd8a8
adrog <adroginski@gmail.com>
2023-01-20 00:22:28 +0100

    Dzień 5 - Praca domowa z Gwiazdką

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,35 +1,67 @@
-﻿string name = "Ewa";
-string maleSuffix = "-latek";
-string femaleSuffix = "-latka";
-bool woman = true;
-var age = 33;
+﻿var Adam = 0;
+var Beata = 0;
+var Cezary = 0;
+var Daria = 0;
+var Emil = 0;
+var Franciszek = 0;
 
-
-if (woman && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (name != "Ewa" && woman)
-{
-    Console.WriteLine(age + femaleSuffix + " o imieniu " + name);
-}
-else if (name != "Ewa" && !woman)
-{
-    Console.WriteLine(age + maleSuffix + " o imieniu " + name);
-}
-else if ( age < 18 && !woman)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine(" Pełnoletni Mężczyzna");
-}
+List<string> namesList = new List<string>();
+namesList.Add("Adam");
+namesList.Add("Beata");
+namesList.Add("Cezary");
+namesList.Add("Adam");
+namesList.Add("Emil");
+namesList.Add("Franciszek");
+namesList.Add("Adam");
+namesList.Add("Adam");
+namesList.Add("Daria");
+namesList.Add("Daria");
+namesList.Add("Emil");
+namesList.Add("Daria");
+namesList.Add("Daria");
+namesList.Add("Daria");
+namesList.Add("Emil");
+namesList.Add("Emil");
+namesList.Add("Daria");
+namesList.Add("Franciszek");
+namesList.Add("Daria");
+namesList.Add("Adam");
 
 
 
+foreach ( var name in namesList)
+{
+    if (name == "Adam")
+    {
+        Adam++;
+    }
+    else if (name == "Beata")
+    {
+        Beata++;
+    }
+    else if (name == "Cezary")
+    {
+        Cezary++;
+    }
+    else if (name == "Daria")
+    {
+        Daria++;
+    }
+    else if (name == "Emil")
+    {
+        Emil++;
+    }
+    else
+    {
+        Franciszek++;
+    }
+}
+
+
+Console.WriteLine("Adam:       " + Adam);
+Console.WriteLine("Beata:      " + Beata);
+Console.WriteLine("Cezary:     " + Cezary);
+Console.WriteLine("Daria:      " + Daria);
+Console.WriteLine("Emil:       " + Emil);
+Console.WriteLine("Franciszek: " + Franciszek);
 
