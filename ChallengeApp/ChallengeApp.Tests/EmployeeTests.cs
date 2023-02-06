07324f021c797413e0e167c799bd06a95fd359a9
adrog <adroginski@gmail.com>
2023-02-06 21:47:48 +0100

    Dzień 8 - Sorted tests

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -1,41 +1,41 @@
 using System.Reflection.Metadata;
 
-namespace ChallengeApp.Tests
+namespace ChallengeApp.Tests;
+
+
+public class EmployeeTests
 {
-    public class Tests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+    }
 
-        [Test]
-        public void AddScorePointsCollectedByUser_ReturnsCorrectResult()
-        {
-           
-            var user = new Employee("Marek","Balski", "22");
-            user.AddScore(5);
-            user.AddScore(6);
-            user.AddScore(7);
+    [Test]
+    public void AddScorePointsCollectedByUser_ReturnsCorrectResult()
+    {
+       
+        var user = new Employee("Marek","Balski", "22");
+        user.AddScore(5);
+        user.AddScore(6);
+        user.AddScore(7);
 
 
-            int result = user.Result;
+        int result = user.Result;
 
-            Assert.AreEqual(18, result);
-        }
-        [Test]
-        public void PenaltyPointsCollectedByUser_ReturnsCorrectResult()
-        {
+        Assert.AreEqual(18, result);
+    }
+    [Test]
+    public void PenaltyPointsCollectedByUser_ReturnsCorrectResult()
+    {
 
-            var user = new Employee("Daniel", "Kot", "17");
-            user.AddScore(5);
-            user.AddScore(6);
-            user.PenaltyPoints(10);
+        var user = new Employee("Daniel", "Kot", "17");
+        user.AddScore(5);
+        user.AddScore(6);
+        user.PenaltyPoints(10);
 
 
-            int result = user.Result;
+        int result = user.Result;
 
-            Assert.AreEqual(1, result);
-        }
+        Assert.AreEqual(1, result);
     }
 }
---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,57 @@
+﻿
+
+namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+
+        public void GetIntValues_ShouldReturnEqual()
+        {
+            int number1 = 7;
+            int number2 = 7;
+
+            Assert.AreEqual(number1, number2);
+
+        }
+
+        [Test]
+
+        public void GetFloatValues_ShouldReturnEqual()
+        {
+            float firstFloat = 3.44f;
+            float secondFloat = 3.44f;
+
+            Assert.AreEqual(firstFloat, secondFloat);
+
+        }
+
+        [Test]
+
+        public void GetStringValues_ShouldReturnDifferent()
+        {
+            string name1 ="Bartek";
+            string name2 = "Igor";
+
+            Assert.AreNotEqual(name1, name2);
+
+        }
+
+        [Test]
+
+        public void GetTwoEmployess_ShouldReturnTwoDifferentObjects()
+        {
+            var employee1 = GetEmployee("Michał", "Kowalski", "22");
+            var employee2 = GetEmployee("Michał", "Kowalski", "22");
+
+            Assert.AreNotEqual(employee1, employee2);
+
+        }
+
+        private Employee GetEmployee(string name, string surname, string age)
+        {
+            return new Employee(name, surname, age);
+        }
+
+    }
+}
